5734982482d2cef2478c4011b5e241c211ab90aa
Rafael Critter <critterrafael@gmail.com>
2026-01-08 20:18:10 -0300

    Add MySQL database configuration

--- start.sh
+++ start.sh
@@ -4,5 +4,12 @@ set -e
 # Remover locks antigos
 rm -f /var/run/apache2/apache2.pid
 
+# Exportar variáveis de ambiente
+export ESPOCRM_DATABASE_HOST=${ESPOCRM_DATABASE_HOST:-mysql.railway.internal}
+export ESPOCRM_DATABASE_PORT=${ESPOCRM_DATABASE_PORT:-3306}
+export ESPOCRM_DATABASE_NAME=${ESPOCRM_DATABASE_NAME:-railway}
+export ESPOCRM_DATABASE_USER=${ESPOCRM_DATABASE_USER:-root}
+export ESPOCRM_DATABASE_PASSWORD=${ESPOCRM_DATABASE_PASSWORD}
+
 # Iniciar Apache em foreground
 exec apache2-foreground
